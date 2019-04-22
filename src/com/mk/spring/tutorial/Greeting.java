b165aaf38d0614abf67e499f4c3b6d2b6fe39561
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-04-22 18:15:13 +0530

    constructor arg

--- src/com/mk/spring/tutorial/Greeting.java
+++ src/com/mk/spring/tutorial/Greeting.java
@@ -2,12 +2,19 @@ package com.mk.spring.tutorial;
 
 public class Greeting {
     private String message;
+    public Greeting() {
+    }
+    public Greeting(String message) {
+        System.out.println("Constructor called!!");
+        this.message = message;
+    }
 
     public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
+        System.out.println("Setter Called");
         this.message = message;
     }
 }
--- src/com/mk/spring/tutorial/Main.java
+++ src/com/mk/spring/tutorial/Main.java
@@ -5,12 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         standard();
-        springSetterExample();
+        springSetterExample(applicationContext);
+        springConstructorExample(applicationContext);
     }
 
-    private static void springSetterExample() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    private static void springConstructorExample(ApplicationContext applicationContext) {
+        Greeting secondGreet = applicationContext.getBean("second", Greeting.class);
+        System.out.println(secondGreet.getMessage());
+    }
+
+    private static void springSetterExample(ApplicationContext applicationContext) {
         Greeting firstGreet = applicationContext.getBean("first", Greeting.class);
         System.out.println(firstGreet.getMessage());
     }
